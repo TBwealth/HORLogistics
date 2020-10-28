8577339702bbbc87c32e5de0eeafdb9ffba82650
Onaopemipo <philipmuyiwa@gmail.com>
2020-10-28 14:20:26 +0100

    Payment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-   {path: '', redirectTo: 'home', pathMatch: 'full'},
+ {path: '', redirectTo: 'payment', pathMatch: 'full'},
+  // {path: '', redirectTo: 'home', pathMatch: 'full'},
 //  {path: '', redirectTo: 'localdelivery/bookingoptions', pathMatch: 'full'},
-  // {path: '', redirectTo: 'localdelivery', pathMatch: 'full'},
+ //{path: '', redirectTo: 'localdelivery', pathMatch: 'full'},
  
   {
     path: 'login',
