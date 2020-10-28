f2846f47fe8bd40cd42ee66e524acdddf00954e7
Ridwan Olawumi <ridwan@smartace.ng>
2020-10-28 13:31:38 +0100

    Fixed issue in service

--- src/app/_services/service-proxies.ts
+++ src/app/_services/service-proxies.ts
@@ -592,62 +592,6 @@ export class ApiServiceProxy {
         }
         return _observableOf<boolean>(<any>null);
     }
-
-    /**
-     * @return OK
-     */
-    localDeliveryTypesGet(): Observable<DeliveryType[]> {
-        let url_ = this.baseUrl + "/api/local-delivery-types";
-        url_ = url_.replace(/[?&]$/, "");
-
-        let options_ : any = {
-            observe: "response",
-            responseType: "blob",
-            headers: new HttpHeaders({
-                "Accept": "application/json"
-            })
-        };
-
-        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
-            return this.processLocalDeliveryTypesGet(response_);
-        })).pipe(_observableCatch((response_: any) => {
-            if (response_ instanceof HttpResponseBase) {
-                try {
-                    return this.processLocalDeliveryTypesGet(<any>response_);
-                } catch (e) {
-                    return <Observable<DeliveryType[]>><any>_observableThrow(e);
-                }
-            } else
-                return <Observable<DeliveryType[]>><any>_observableThrow(response_);
-        }));
-    }
-
-    protected processLocalDeliveryTypesGet(response: HttpResponseBase): Observable<DeliveryType[]> {
-        const status = response.status;
-        const responseBlob =
-            response instanceof HttpResponse ? response.body :
-            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
-
-        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
-        if (status === 200) {
-            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
-            let result200: any = null;
-            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
-            if (Array.isArray(resultData200)) {
-                result200 = [] as any;
-                for (let item of resultData200)
-                    result200.push(DeliveryType.fromJS(item));
-            }
-            return _observableOf(result200);
-            }));
-        } else if (status !== 200 && status !== 204) {
-            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
-            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
-            }));
-        }
-        return _observableOf<DeliveryType[]>(<any>null);
-    }
-
     /**
      * @return OK
      */
