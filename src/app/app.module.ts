29554a8c3d1b5ea0e9e826e4dbdfa4d4145e89ca
Ridwan <olarid7852@gmail.com>
2020-11-05 13:16:42 +0000

    Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,11 +38,7 @@ import { AuthenticationService } from './_services/authentication.service';
 import { JwtInterceptor } from './_services/jwt.interceptor';
 import { PageStructureComponent } from './components/page-structure/page-structure.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
-<<<<<<< HEAD
 import { AccountServiceProxy,RegisterServiceProxy,CountriesServiceProxy, ApiServiceProxy, ManageServiceProxy, LocalBookingServiceProxy, LocationsServiceProxy } from './_services/service-proxies';
-=======
-import { LocationsServiceProxy,AccountServiceProxy,RegisterServiceProxy,CountriesServiceProxy, ApiServiceProxy, ManageServiceProxy } from './_services/service-proxies';
->>>>>>> cbe64bf721b777e6e87617754d10497f68789ad5
 import { ChatService } from './_services/chat.service';
 import {MaprouteService} from './_services/maproute.service';
 import { StoreService } from './_services/store.service';
