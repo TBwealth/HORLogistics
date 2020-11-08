71a518d527d2fc0dc311e06efda742def0baed05
Onaopemipo <philipmuyiwa@gmail.com>
2020-11-08 02:13:21 +0100

    Checkout Assit

--- src/app/checkout/multiple/multiple.page.ts
+++ src/app/checkout/multiple/multiple.page.ts
@@ -1,11 +1,14 @@
 import { NeworderComponent } from './../neworder/neworder.component';
 import { Component, OnInit } from '@angular/core';
 import { product_descriptionModel} from './../../_models/checkout';
-import { FormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {Router,ActivatedRoute} from '@angular/router';
 import { NavController,ToastController,AlertController} from '@ionic/angular';
 import { PopoverController } from '@ionic/angular';
 import { Subject } from 'rxjs';
+import { CheckoutassistanceServiceProxy } from 'src/app/_services/service-proxies';
+import { CheckOutAssistanceProductModel,CheckOutAssistanceDTO,CheckoutAssistance, CheckOutAssistanceModel,ObjectResourceOfUserViewModel } from 'src/app/_models/service-models';
+
 
 @Component({
   selector: 'app-multiple',
@@ -24,62 +27,62 @@ export class MultiplePage implements OnInit {
   
   product_descriptionForm :FormGroup;
 
-
+  myorder: CheckOutAssistanceProductModel = new CheckOutAssistanceProductModel ();
   // delivery_details : delivery_details = {};
   // product_description : product_descriptionModel ={};
-  product_desc : product_descriptionModel = {};
 
   orders = {};
+  myForm :FormGroup;
+
+  product_desc = new CheckOutAssistanceProductModel().clone();
+  request_desc = new CheckOutAssistanceModel().clone();
+  all_product_desc: CheckOutAssistanceProductModel[];
   
+  checkOutAsst = new CheckOutAssistanceDTO().clone();
 
   constructor(private toastCtrl: ToastController,
     private router: Router,
     private navCtrl: NavController,
     public popoverController: PopoverController,
-    private activatedroute: ActivatedRoute) { }
+    private activatedroute: ActivatedRoute,
+    private checkout: CheckoutassistanceServiceProxy) { }
 
   
-      async addOrder(ev: any) {
-        const popover = await this.popoverController.create({
+      // async addOrder(ev: any) {
+      //   const popover = await this.popoverController.create({
+      //     component: NeworderComponent,
+      //     cssClass: 'my-custom-class',
+      //     event: ev,
+      //     translucent: true
+      //   });
+      //   return await popover.present();
+      // }
+      async addOrder(){
+        const subject = new Subject<boolean>()
+        const modal = await this.popoverController.create({
           component: NeworderComponent,
           cssClass: 'my-custom-class',
-          event: ev,
-          translucent: true
+          componentProps: {
+            subject
+          }
         });
-        return await popover.present();
+        subject.subscribe(val => {
+          modal.dismiss()
+          if(val){
+            this.clearForm()
+          } else {
+            this.gotoSummary()
+          }
+        })
+        return await modal.present();
       }
-  //   }
-  //   async showAddNewOrderModal(){
-  //   const subject = new Subject<boolean>()
-  //   const modal = await this.popoverController.create({
-  //     component: NeworderComponent,
-  //     cssClass: 'my-custom-class',
-  //     componentProps: {
-  //       subject
-  //     }
-  //   });
-  //   subject.subscribe(val => {
-  //     modal.dismiss()
-  //     if(val){
-  //       this.clearForm()
-  //     } else {
-  //       this.gotoSummary()
-  //     }
-  //   })
-  //   return await modal.present();
-  // }
-
-  // clearForm(){
-    
-  // }
-  // gotoSummary(){
-  //   // this.router.navigate(['localdelivery/review-booking'])
-  // }
 
   ngOnInit() {
   }
 
-
+  authuserdetails = new ObjectResourceOfUserViewModel().clone();
+  // myorder: CheckOutAssistanceProductModel = new CheckOutAssistanceProductModel ();
+  
   
   home_delivery($option){
     this.homedelivery = $option;
@@ -103,28 +106,24 @@ myfunction($val){
 
 validateProductDescriptionForm(){
 
-if(this.product_desc.product_name &&
-   this.product_desc.product_url &&
-    this.product_desc.delivery_type &&
-     this.product_desc.color && this.product_desc.size){
-  this.deliveryDetailsPanel = false;
-}else{
-
-  //this.deliveryDetailsPanel = true;
-// this.productDescriptionPanel = true;
-}
-}
-
-validateDeliveryDetailsForm(){
-  console.log(this.product_desc.home_delivery);
-if(this.product_desc.home_delivery &&
-  this.product_desc.location &&
-   this.product_desc.state){
-       
- this.addNewPanel = false;
-}
-}
-
+  if(this.product_desc.name &&
+     this.product_desc.url &&
+      this.product_desc.delivery &&
+      this.product_desc.quantity &&
+       this.product_desc.color && this.product_desc.size){
+    this.deliveryDetailsPanel = false;
+  }
+  }
+  
+  validateDeliveryDetailsForm(){
+  if(this.request_desc.customerAddress &&
+     this.request_desc.deliveryLocationId &&
+     this.request_desc.shipToState){
+         
+      this.myorder = this.product_desc;
+      //console.log(this.myorder)
+  }
+  }
 
 myoption(event) {
   this.homedelivery = event.detail.value;
@@ -143,6 +142,92 @@ logvalue(event){
   console.log(event)
 }
 
+async addNewProduct(){
+  const subject = new Subject<boolean>()
+  const modal = await this.popoverController.create({
+    component: NeworderComponent,
+    cssClass: 'my-custom-class',
+    componentProps: {
+      subject
+    }
+  });
+  subject.subscribe(val => {
+    modal.dismiss()
+    if(val){
+      this.clearForm()
+    } else {
+      this.gotoSummary()
+    }
+  })
+  return await modal.present();
+}
 
+clearForm(){
+  this.saveOrder()
+  this.productDescriptionPanel = false
+  this.deliveryDetailsPanel  = false
+  this.activetab = null
+}
+
+onSubmit(){
+  let productDetails = new CheckOutAssistanceProductModel()
+  //Product details
+  productDetails.name = this.product_desc.name;
+  productDetails.quantity = this.product_desc.quantity;
+  productDetails.size = this.product_desc.size;
+  productDetails.color = this.product_desc.color;
+  productDetails.url = this.product_desc.url;
+  productDetails.delivery = this.product_desc.delivery
+  productDetails.itemNumber = this.product_desc.itemNumber
+  productDetails.comment = this.product_desc.comment;
+  productDetails.style = this.product_desc.style;
+
+  //Delivery Details
+  let RequestDetails = new CheckOutAssistanceModel();
+  RequestDetails.checkoutProcessing = this.request_desc.checkoutProcessing;
+  RequestDetails.customerAddress = this.authuserdetails.data.customer.homeAddress;
+  RequestDetails.customerName = this.authuserdetails.data.customer.fullName;
+  RequestDetails.customerPhone = this.authuserdetails.data.user.phoneNumber;
+  RequestDetails.shipToState = this.request_desc.shipToState;
+  RequestDetails.pickupCenter = this.request_desc.pickupCenter;
+  RequestDetails.deliveryLocationId = this.request_desc.deliveryLocationId;
+
+  this.checkOutAsst.products.push(productDetails);
+  this.checkOutAsst.request = RequestDetails;
+  console.log(this.checkOutAsst);
+
+  //  let singleProduct = new 
+//     console.log(myForm.value);
+//     this.checkout.create(myForm.value).subscribe(data => {})
+  
+}
+
+
+saveOrder() {
+  this.gotoSummary();
+  
+}
+
+// submitBooking(){
+//   if(this.singleDelivery){
+//     this.gotoSummary()
+//   } else {
+//     this.showAddNewBookingModal()
+//   }
+// }
+
+gotoSummary(){
+  this.onSubmit()
+    //this.store.saveBookings(this.checkOutAsst)
+    this.router.navigate(['localdelivery/review-booking'])
+
+}
+
+// onSubmit(myForm){
+   
+//   console.log(myForm.value);
+//   this.checkout.create(myForm.value).subscribe(data => {})
+
+// }
 
 }
