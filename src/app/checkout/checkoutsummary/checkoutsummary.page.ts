df20ac79bf21d9ec7863ebd10735e0d37d0802a2
Temitope Badmus <tope@smartace.ng>
2020-11-18 14:52:40 +0100

    update

--- src/app/checkout/checkoutsummary/checkoutsummary.page.ts
+++ src/app/checkout/checkoutsummary/checkoutsummary.page.ts
@@ -1,5 +1,7 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
-import { NavController} from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-checkoutsummary',
@@ -7,10 +9,30 @@ import { NavController} from '@ionic/angular';
   styleUrls: ['./checkoutsummary.page.scss'],
 })
 export class CheckoutsummaryPage implements OnInit {
+checkoutassist: any = "";
+loading: any=''
+  constructor(private navCtrl: NavController,
+    private activatedroute: ActivatedRoute,
+    private router: Router,
+    private loadspinner : LoadingController) { }
 
-  constructor(private navCtrl: NavController) { }
-
-  ngOnInit() {
+ async ngOnInit() {
+  this.loading = await this.loadspinner.create({
+    message: "please wait...",
+    translucent: true,
+    spinner: "bubbles",
+  });
+  await this.loading.present();
+    this.activatedroute.queryParams.subscribe(data=>{
+      if(data.details){
+this.checkoutassist = JSON.parse(data.details);
+this.loading.dismiss()
+console.log(this.checkoutassist);
+      }else{
+        this.loading.dismiss()
+this.router.navigate(['checkoutlist'])
+      }
+    })
   }
 
   goback(){
